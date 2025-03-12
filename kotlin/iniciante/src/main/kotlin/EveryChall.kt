0263842a3793a9d0f309d730c6d075e02e50aa98
jedin01 <abnerjaredejede@gmail.com>
2025-03-12 11:28:11 +0100

    mar03 11:28

--- kotlin/iniciante/src/main/kotlin/EveryChall.kt
+++ kotlin/iniciante/src/main/kotlin/EveryChall.kt
@@ -1,5 +1,48 @@
 package org.example
 
+import java.lang.reflect.Array
+
 class EveryChall {
 
+    fun print():Unit{
+        print("Hello, Wolrd!")
+    }
+
+    fun sum():Unit{
+        val a: Int = readln().toInt()
+        val b: Int = readln().toInt()
+        val x: Int = a+b
+        println("X = "+x)
+    }
+    fun encrypt():Unit{
+        fun main() {
+            val times = readln().toInt()
+            val input = ArrayList<String>()
+
+            for (i in 1..times) {
+                val line = readln()
+                input.add(line)
+            }
+
+            for (original in input) {
+                val result = StringBuilder()
+
+                original.forEach { c ->
+                    if (c.isLetter()) {
+                        result.append((c.code + 3).toChar())
+                    } else {
+                        result.append(c)
+                    }
+                }
+
+                result.reverse()
+
+                val mid = result.length / 2
+                for (i in mid until result.length) {
+                    result.setCharAt(i, (result[i].code - 1).toChar())
+                }
+                println(result.toString())
+            }
+        }
+    }
 }
--- kotlin/iniciante/src/main/kotlin/Main.kt
+++ kotlin/iniciante/src/main/kotlin/Main.kt
@@ -1,5 +1,33 @@
 package org.example
 
 fun main() {
-    print(message = "Hello, World!")
+    val times:Int = readln().toInt()
+    val input:ArrayList<String> = ArrayList()
+    for (i in 1..times){
+        var a:String = readln()
+        input.add(a)
+    }
+    for (a in input){
+        var result:StringBuilder = StringBuilder()
+        a.forEach { c: Char ->
+            run {
+                if (!c.isLetter()){
+                    result.append((c.code).toChar())
+                }else{
+                    result.append((c.code + 3).toChar())
+                }
+            }
+        }
+        result.reverse()
+        val a:String = result.substring(result.length/2)
+        result.delete(result.length/2,result.length)
+        a.forEach { c: Char ->
+            run {
+                result.append((c.code-1).toChar())
+            }
+        }
+
+        println(result)
+    }
+
 }
